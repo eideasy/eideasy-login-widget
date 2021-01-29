619a60bdac88679a29a8a8bd4bd0cde3e496216e
matsjoonas <mj@codelight.eu>
2021-01-29 16:28:43 +0200

    Add empty header component

---
+++ src/features/common/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="Header">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style lang="scss" scoped>
+.Header {
+  border-bottom: $border-default;
+  min-height: 71px;
+  padding: $spacer-1 $spacer-4;
+  background-color: $gray-400;
+}
+
+</style>
--- src/features/common/Layout.vue
+++ src/features/common/Layout.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="Layout">
+    <Header></Header>
     <div class="Main">
       <component :is="currentView"></component>
     </div>
@@ -13,12 +14,14 @@
 import {store, mutations} from '../../store';
 import MethodSelection from '../views/MethodSelection.vue';
 import Footer from './Footer';
+import Header from './Header';
 
 export default {
   name: 'Layout',
   components: {
     MethodSelection,
     Footer,
+    Header,
   },
   data: () => ({
     currentView: store.currentView,
@@ -34,9 +37,10 @@ export default {
     background-color: $gray-100;
     border-radius: $border-radius;
     border: 1px solid $border-color;
+    overflow: hidden;
   }
 
   .Main {
-    padding: $spacer-4 $spacer-10;
+    padding: $spacer-6 $spacer-10;
   }
 </style>
